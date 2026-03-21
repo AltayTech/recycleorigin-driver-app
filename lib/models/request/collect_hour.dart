e7dabcf5397d4dbb7dbffe8a8eaca7e2426992b3
Reza Mojed <31512322+AltayTech@users.noreply.github.com>
2026-03-21 21:45:38 +0300

    fixed: repeat collect time problem

--- lib/models/request/collect_hour.dart
+++ lib/models/request/collect_hour.dart
@@ -4,18 +4,21 @@ class CollectHour with ChangeNotifier {
   final String start;
   final String end;
   final bool collect_hour_status;
+  final String repeat_pattern;
 
   CollectHour({
     required this.end,
     required this.start,
     required this.collect_hour_status,
+    this.repeat_pattern = 'daily',
   });
 
   factory CollectHour.fromJson(Map<String, dynamic> parsedJson) {
     return CollectHour(
-      start: parsedJson['start'],
-      end: parsedJson['end'],
-      collect_hour_status: parsedJson['collect_hour_status'],
+      start: parsedJson['start']?.toString() ?? '',
+      end: parsedJson['end']?.toString() ?? '',
+      collect_hour_status: parsedJson['collect_hour_status'] as bool? ?? true,
+      repeat_pattern: parsedJson['repeat_pattern']?.toString() ?? 'daily',
     );
   }
 
@@ -23,5 +26,6 @@ class CollectHour with ChangeNotifier {
         'start': start,
         'end': end,
         'collect_hour_status': collect_hour_status,
+        'repeat_pattern': repeat_pattern,
       };
 }
